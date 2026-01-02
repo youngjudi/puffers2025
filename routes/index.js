ad76efd7f3cc010cb80ace806226d4d6771ca236
tikyydc <ervinaksizov@gmail.com>
2026-01-03 00:25:35 +0300

    puffers_4_3

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* Страница Джордана*/
+/* Страница Джордана */
 router.get('/jordan', function(req, res, next) {
-    res.send("<h1>Страница Пуховика Jordan</h1>")
+    res.render('puffer', {
+        title: "Puffer Jordan",
+        picture: "images/jordan.jpg",
+        desc: "Пуховики Jordan — это стильные и тёплые куртки с логотипом Jumpman, часто с синтетическим утеплителем или пухом. Они водоотталкивающие, объёмные, идеальны для холодной погоды."
+    });
 });
 /* Страница Найка */
 router.get('/nike', function(req, res, next) {
-    res.send("<h1>Страница пуховика Nike</h1>")
+    res.render('puffer', {
+        title: "Puffer Nike",
+        picture: "images/nike.jpg",
+        desc: "Пуховики Nike сочетают спорт и повседневность, с технологиями Therma-FIT, Storm-FIT для защиты от ветра и влаги. Утеплитель синтетический или пуховый, водоотталкивающая ткань, лёгкие и тёплые."
+    });
 });
-
 /* Страница Аликса */
 router.get('/alyx', function(req, res, next) {
-    res.send("<h1>Страница пуховика Alyx</h1>")
+    res.render('puffer', {
+        title: "Puffer Alyx",
+        picture: "images/alyx.jpg",
+        desc: "Пуховики Alyx — авангардная высокая мода от Matthew Williams, с фирменными пряжками-роллеркоастерами, нейлоновой тканью и стёганым дизайном."
+    });
 });
+
 module.exports = router;
