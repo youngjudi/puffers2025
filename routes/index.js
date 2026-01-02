0be7a2be7b502d62ff46255fb5289e8680eff5fb
tikyydc <ervinaksizov@gmail.com>
2026-01-03 01:44:20 +0300

    puffers_9_2

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ res.cookie('greeting', 'Hi!!!').render('index', { title: 'Express' });
 });
+
 
 /* Страница Джордана */
 router.get('/jordan', function(req, res, next) {
