cc18f0e5c297261c7de6d480806b52955267cb7c
tikyydc <ervinaksizov@gmail.com>
2026-01-03 01:19:02 +0300

    puffers_8_2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/puffers2025')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var indexPuffers = require('./routes/puffers');
 var app = express();
 
 // view engine setup
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/puffers', indexPuffers);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
