d19485f2abbe099cdbd85a6d750555bb72f13d65
youngjudi <artem.kulish5589@gmail.com>
2026-01-03 02:42:04 +0300

    puffers_10_8

--- routes/puffers.js
+++ routes/puffers.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var Puffers = require('../models/puffer').Puffers;
+var checkAuth = require("../middlewares/checkAuth.js");
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с puffers');
 });
 /* Страница пуховиков */
-router.get("/:nick", async function(req, res, next) {
+router.get("/:nick", checkAuth, async function(req, res, next) {
    var puffers = await Puffers.find({nick: req.params.nick});
    console.log(puffers)
    if(!puffers.length) return next(new Error("Нет такого пуховика в нашем магазине"))
