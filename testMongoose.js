ced6e2b07487f173b909ad5f28afaca350c4add9
tikyydc <ervinaksizov@gmail.com>
2026-01-03 01:03:29 +0300

    puffers_7_2

--- testMongoose.js
+++ testMongoose.js
@@ -1,9 +1,19 @@
+
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
 
+const jacketSchema = new mongoose.Schema({ 
+  name: String 
+});
 
-const jacket = mongoose.model('Puffers', { name: String });
+jacketSchema.methods.show = function () {
+  console.log(`${this.name} — в магазине прямо сейчас!`);
+};
 
+const Jacket = mongoose.model('Puffers', jacketSchema);
 
-const nike = new jacket({ name: 'nike' });
-nike.save().then(() => console.log('Пуховик сохранён!)'));
+const nike = new Jacket({ name: 'Puffer Nike' });
+nike.save().then(() => {
+  console.log('Пуховик сохранён!');
+  nike.show();  // ← вот здесь сработает наш метод
+});
