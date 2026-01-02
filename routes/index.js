c2cfc787d8d6247fee56694d14b2ebfbdc1215af
tikyydc <ervinaksizov@gmail.com>
2026-01-03 02:02:22 +0300

    puffers_10_1

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express',  counter:req.session.counter });
 });
+/* GET login/registration page. */
+router.get('/logreg', function(req, res, next) {
+ res.render('logreg',{title: 'Вход'});
+ });
 
 module.exports = router;
