09841a92bc5e320535aec448b7d19fe9bea79a6d
tikyydc <ervinaksizov@gmail.com>
2026-01-03 01:50:20 +0300

    puffers_9_3

--- app.js
+++ app.js
@@ -22,13 +22,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+var MongoStore = require('connect-mongo').default;
 app.use(session({
  secret: "ThreePuffers",
  cookie:{maxAge:60*1000},
  proxy: true,
  resave: true,
- saveUninitialized: true
+ saveUninitialized: true,
+ store: MongoStore.create({mongoUrl: 'mongodb://localhost/puffers2025'})
 }))
 
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -3,33 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
- res.cookie('greeting', 'Hi!!!').render('index', { title: 'Express' });
+ req.session.greeting = "Hi!!!";
+ res.render('index', { title: 'Express' });
 });
 
-
-/* Страница Джордана */
-router.get('/jordan', function(req, res, next) {
-    res.render('puffer', {
-        title: "Puffer Jordan",
-        picture: "images/jordan.jpg",
-        desc: "Пуховики Jordan — это стильные и тёплые куртки с логотипом Jumpman, часто с синтетическим утеплителем или пухом. Они водоотталкивающие, объёмные, идеальны для холодной погоды."
-    });
-});
-/* Страница Найка */
-router.get('/nike', function(req, res, next) {
-    res.render('puffer', {
-        title: "Puffer Nike",
-        picture: "images/nike.jpg",
-        desc: "Пуховики Nike сочетают спорт и повседневность, с технологиями Therma-FIT, Storm-FIT для защиты от ветра и влаги. Утеплитель синтетический или пуховый, водоотталкивающая ткань, лёгкие и тёплые."
-    });
-});
-/* Страница Аликса */
-router.get('/alyx', function(req, res, next) {
-    res.render('puffer', {
-        title: "Puffer Alyx",
-        picture: "images/alyx.jpg",
-        desc: "Пуховики Alyx — авангардная высокая мода от Matthew Williams, с фирменными пряжками-роллеркоастерами, нейлоновой тканью и стёганым дизайном."
-    });
-});
 
 module.exports = router;
