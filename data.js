2e6b6a19b670c7c14f86d501923a93f08b98f6be
tikyydc <ervinaksizov@gmail.com>
2026-01-03 00:44:51 +0300

    puffers_6_2

---
+++ data.js
@@ -0,0 +1,22 @@
+var data = [{
+   title: 'Puffer Jordan',
+   nick: 'jordan',
+   avatar: '/images/jordan.jpg',
+   desc: 'Пуховики Jordan — это стильные и тёплые куртки с логотипом Jumpman, часто с синтетическим утеплителем или пухом. Они водоотталкивающие, объёмные, идеальны для холодной погоды.'
+},
+{
+   title: 'Puffer Nike',
+   nick: 'nike',
+   avatar: '/images/nike.jpg',
+   desc: 'Пуховики Nike сочетают спорт и повседневность, с технологиями Therma-FIT, Storm-FIT для защиты от ветра и влаги. Утеплитель синтетический или пуховый, водоотталкивающая ткань, лёгкие и тёплые.'
+},
+{
+   title: 'Puffer Alyx',
+   nick: 'alyx',
+   avatar: '/images/alyx.jpg',
+   desc: 'Пуховики Alyx — авангардная высокая мода от Matthew Williams, с фирменными пряжками-роллеркоастерами, нейлоновой тканью и стёганым дизайном.'
+}
+];
+
+
+module.exports.data = data;
