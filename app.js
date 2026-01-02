289704bf5f1876228f2d59ad9a130037ea37699d
tikyydc <ervinaksizov@gmail.com>
2026-01-03 01:53:25 +0300

    puffers_9_4

--- app.js
+++ app.js
@@ -32,6 +32,12 @@ app.use(session({
  store: MongoStore.create({mongoUrl: 'mongodb://localhost/puffers2025'})
 }))
 
+app.use(function(req,res,next){
+ req.session.counter = req.session.counter + 1 || 1
+ next()
+ })
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/puffers', indexPuffers);
